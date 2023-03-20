7c62ef0ae91cbc43f2fbc3454f9ffa3cf214e1c0
ican-graphics-01 <lankaican@gmail.com>
2023-03-20 10:29:23 +0530

    style created

--- src/components/HeroSectionTwo.vue
+++ src/components/HeroSectionTwo.vue
@@ -4,13 +4,13 @@
 
             <div class="py-24 overflow-hidden sm:py-28">
 
-                <div class="px-6 mx-auto max-w-7xl lg:px-8">
+                <div class="max-w-5xl px-6 mx-auto lg:px-8">
                     <div class="grid max-w-2xl grid-cols-1 mx-auto gap-y-16 gap-x-8 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-                      <div class="lg:pr-8 lg:pt-4">
+                      <div class="lg:pr-6 lg:pt-4">
                         <div class="lg:max-w-lg">
                           <h2 class="text-base font-semibold leading-7 text-indigo-600" href="#">Download iCan Mall</h2>
-                          <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">iCan Mall</p>
-                          <p class="mt-6 text-lg leading-8 text-gray-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</p>
+                          <p class="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">iCan Mall</p>
+                          <p class="mt-6 text-lg leading-8 text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</p>
               
                           <div class="flex items-center justify-center mt-3 mb-5 gap-y-4">
                               <img @click="playstore" src="../assets/play.png" class="w-32 h-10 cursor-pointer" alt="">
@@ -28,7 +28,7 @@
               
                         </div>
                       </div>
-                      <img src="../assets/img1.png" alt="Product screenshot" class="w-[32rem] max-w-none rounded-md ring-gray-400/10 sm:w-[48rem] md:-ml-4 lg:-ml-0" width="1200" height="800" />
+                      <img src="../assets/img1.png" alt="Product screenshot" class="w-[18rem] mt-16 max-w-none rounded-md ring-gray-400/10 sm:w-[24rem] md:-ml-4 lg:-ml-0" width="1200" height="800" />
                     </div>
                 </div>
             </div>   
@@ -43,8 +43,8 @@
                       <div class="lg:pr-8 lg:pt-4">
                         <div class="lg:max-w-lg">
                           <h2 class="text-base font-semibold leading-7 text-indigo-600" href="#">Download iCan Mall</h2>
-                          <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">iCan Mall</p>
-                          <p class="mt-6 text-lg leading-8 text-gray-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</p>
+                          <p class="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">iCan Mall</p>
+                          <p class="mt-6 text-lg leading-8 text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</p>
               
                           <div class="flex items-center justify-center mt-3 mb-5 gap-y-4">
                               <img @click="playstore" src="../assets/play.png" class="w-32 h-10 cursor-pointer" alt="">
@@ -62,7 +62,7 @@
               
                         </div>
                       </div>
-                      <img src="../assets/img1.png" alt="Product screenshot" class="w-[32rem] max-w-none rounded-md ring-gray-400/10 sm:w-[48rem] md:-ml-4 lg:-ml-0" width="1200" height="800" />
+                      <img src="../assets/img1.png" alt="Product screenshot" class="w-[18rem] mt-16 max-w-none rounded-md ring-gray-400/10 sm:w-[24rem] md:-ml-4 lg:-ml-0" width="1200" height="800" />
                     </div>
                 </div>
         </div>
